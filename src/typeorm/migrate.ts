cdbee2797308d5bd63f782c542eaca193728ff05
Theo Sun <theo.sun@outlook.com>
2021-02-20 19:10:12 +0800

    chore: refactor

--- src/typeorm/migrate.ts
+++ src/typeorm/migrate.ts
@@ -1,11 +1,11 @@
 import { ConnectionOptions } from "typeorm";
-import { CDSMySQLConnection, CDSMySQLSchemaBuilder } from "./mysql";
+import { CDSMySQLConnection } from "./mysql";
 
 export async function migrate(connectionOptions: ConnectionOptions) {
   const conn = new CDSMySQLConnection(connectionOptions);
   try {
     await conn.connect();
-    const builder = new CDSMySQLSchemaBuilder(conn);
+    const builder = conn.driver.createSchemaBuilder();
     await builder.build(); // execute build
   } finally {
     if (conn.isConnected) {
