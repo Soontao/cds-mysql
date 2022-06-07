6643feb9cba708ab551dbf9e4c7881cf01fab744
Theo Sun <theo.sun@outlook.com>
2022-06-07 22:10:19 +0800

    fix: tidb ci

---
+++ docker/tidb/create_user.js
@@ -0,0 +1,21 @@
+(
+  async () => {
+    try {
+      const mysql = require('mysql2/promise');
+      const conn = await mysql.createConnection({
+        user: 'root',
+        host: '127.0.0.1',
+        port: 4000,
+      })
+
+      await conn.execute(`CREATE USER 'cdstest'@'%' IDENTIFIED BY 'cdstest'; GRANT ALL on cdstest.* to 'cdstest'@'%';`)
+
+      conn.destroy()
+      process.exit(0)
+    } catch (error) {
+      console.error('create user failed: ', error)
+      process.exit(1)
+    }
+
+  }
+)();
