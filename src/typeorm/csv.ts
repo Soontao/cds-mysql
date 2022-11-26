fc2d6eee7bb325e46663f2bbd4f0d0f45ef299bf
Theo Sun <theo.sun.github@outlook.com>
2022-11-26 18:11:57 +0800

    refactor: csv provision

--- src/typeorm/csv.ts
+++ src/typeorm/csv.ts
@@ -1,4 +1,5 @@
 /* eslint-disable prefer-const */
+import { Semaphore } from "@newdash/newdash/functional/Semaphore";
 import flattenDeep from "@newdash/newdash/flattenDeep";
 import uniq from "@newdash/newdash/uniq";
 import { cwdRequire, cwdRequireCDS, fuzzy, LinkedModel, memorized } from "cds-internal-tool";
@@ -85,242 +86,255 @@ export async function migrateData(
     )
   );
 
-  if (csvList.length > 0) {
+  if (csvList.length === 0) {
+    return;
+  }
 
-    const connection = await createConnection(credential as ConnectionOptions);
+  const connection = await createConnection(credential as ConnectionOptions);
 
-    try {
+  try {
 
-      await connection.beginTransaction();
+    await connection.beginTransaction();
 
-      logger.info("start provisioning data with CSV files");
+    logger.info("start provisioning data with CSV files");
 
-      const [csvHistory] = await connection
-        .query(`SHOW TABLES LIKE '${TABLE_CSV_HISTORY}'`) as any as Array<any>;
+    const [csvHistory] = await connection
+      .query(`SHOW TABLES LIKE '${TABLE_CSV_HISTORY}'`) as any as Array<any>;
 
-      const withHistoryTable = csvHistory?.length > 0;
+    const withHistoryTable = csvHistory?.length > 0;
 
-      for (const csvFile of csvList) {
-        const filename = path.basename(csvFile, ".csv");
-        const entityName = filename.replace(/[_-]/g, "."); // name_space_entity.csv -> name.space.entity
+    if (!withHistoryTable) {
+      logger.info("csv history table is not ready, skip duplicated migration check");
+    }
 
-        const entityModel = fuzzy.findEntity(filename, model);
+    for (const csvFile of csvList) {
+      const filename = path.basename(csvFile, ".csv");
+      const entityName = filename.replace(/[_-]/g, "."); // name_space_entity.csv -> name.space.entity
 
-        if (entityModel === undefined) {
-          logger.warn(entityName, "is not in the model");
-          continue;
-        }
+      const entityModel = fuzzy.findEntity(filename, model);
 
-        // check the CSV has been provisioned or not
-        const csvFileHash = await sha256(csvFile);
+      if (entityModel === undefined) {
+        logger.warn(entityName, "is not in the model");
+        continue;
+      }
 
-        // if csv history table was setup for target tenant
-        if (withHistoryTable) {
+      // check the CSV has been provisioned or not
+      const csvFileHash = await sha256(csvFile);
 
-          const [csvFileHashExists] = await connection.query(
-            "SELECT ENTITY, HASH FROM ?? WHERE ENTITY = ? FOR UPDATE",
-            [TABLE_CSV_HISTORY, entityModel.name, csvFileHash]
-          ) as any as [Array<{ ENTITY: string, HASH: string }>];
+      // if csv history table was setup for target tenant
+      if (withHistoryTable) {
 
-          if (csvFileHashExists instanceof Array && csvFileHashExists.length > 0) {
-            if (csvFileHashExists[0].HASH === csvFileHash) {
-              logger.info(
-                "file", csvFile.green,
-                "with hash", csvFileHash.substring(32).gray,
-                "has been provisioned before, skip processing"
-              );
-              continue;
-            }
-            else {
-              // TODO: test CSV file change
-              // existed but CSV hash different
-              await connection.query(
-                "UPDATE ?? SET HASH = ? WHERE ENTITY = ?",
-                [TABLE_CSV_HISTORY, csvFileHash, entityModel.name]
-              );
-            }
+        const [csvFileHashExists] = await connection.query(
+          "SELECT ENTITY, HASH FROM ?? WHERE ENTITY = ? FOR UPDATE",
+          [TABLE_CSV_HISTORY, entityModel.name, csvFileHash]
+        ) as any as [Array<{ ENTITY: string, HASH: string }>];
+
+        if (csvFileHashExists instanceof Array && csvFileHashExists.length > 0) {
+          if (csvFileHashExists[0].HASH === csvFileHash) {
+            logger.info(
+              "file", csvFile.green,
+              "with hash", csvFileHash.substring(32).gray,
+              "has been provisioned before, skip processing"
+            );
+            continue;
           }
           else {
+            // TODO: test CSV file change
+            // existed but CSV hash different
             await connection.query(
-              "INSERT INTO ?? (entity, hash) VALUES (?, ?)",
-              [TABLE_CSV_HISTORY, entityModel.name, csvFileHash]
+              "UPDATE ?? SET HASH = ? WHERE ENTITY = ?",
+              [TABLE_CSV_HISTORY, csvFileHash, entityModel.name]
             );
           }
         }
         else {
-          logger.info("csv history table is not ready, skip duplicated migration check");
+          await connection.query(
+            "INSERT INTO ?? (entity, hash) VALUES (?, ?)",
+            [TABLE_CSV_HISTORY, entityModel.name, csvFileHash]
+          );
         }
+      }
 
-        const isPreDeliveryModel = (
-          entityModel.includes?.includes?.("preDelivery") &&
-          entityModel.elements?.["PreDelivery"]?.type === "cds.Boolean"
+      const isPreDeliveryModel = (
+        entityModel.includes?.includes?.("preDelivery") &&
+        entityModel.elements?.["PreDelivery"]?.type === "cds.Boolean"
+      );
+
+      const CSV = cwdRequire("@sap/cds/lib/compile/etc/csv");
+      const entires: Array<Array<string>> = CSV.read(csvFile);
+      const tableName = entityName.replace(/\./g, "_");
+
+      /**
+       * key names of entity
+       */
+      const keys = Object.values(entityModel.elements)
+        .filter((e: any) => e.key === true)
+        .map((e: any) => e.name);
+
+      let [headers, ...rows] = entires;
+
+      // fuzzy mapping element
+      headers = headers.map(header => {
+        const element = fuzzy.findElement(entityModel, header);
+        if (element === undefined) {
+          throw cds.error(
+            `csv file '${csvFile}' column ` +
+            `with header key '${header}' is not found ` +
+            `in the entity '${entityModel.name}'`
+          );
+        }
+        return element?.name;
+      });
+
+      const transformColumnsIndex = Object
+        .values(entityModel.elements)
+        .filter(ele => TRANSPORT_CDS_TYPES.includes(ele.type))
+        .filter(ele => headers.includes(ele.name)) // only process columns in CSV files
+        .map(ele => ({
+          index: headers.indexOf(ele.name),
+          type: ele.type,
+          columnName: ele.name,
+        }));
+
+
+      const existedKeysIndex = headers
+        .filter(header => keys.includes(header))
+        .map(existedKey => headers.indexOf(existedKey));
+
+      if (existedKeysIndex.length !== keys.length) {
+        logger.warn(
+          "file", csvFile.yellow,
+          "do not provide enough primary keys",
+          keys,
+          "could not provision CSV, skip process"
         );
+        continue;
+      }
 
-        const CSV = cwdRequire("@sap/cds/lib/compile/etc/csv");
-        const entires: Array<Array<string>> = CSV.read(csvFile);
-        const tableName = entityName.replace(/\./g, "_");
-
-        /**
-         * key names of entity
-         */
-        const keys = Object.values(entityModel.elements)
-          .filter((e: any) => e.key === true)
-          .map((e: any) => e.name);
-
-        let [headers, ...rows] = entires;
-
-        // fuzzy mapping element
-        headers = headers.map(header => {
-          const element = fuzzy.findElement(entityModel, header);
-          if (element === undefined) {
-            throw cds.error(
-              `csv file '${csvFile}' column ` +
-              `with header key '${header}' is not found ` +
-              `in the entity '${entityModel.name}'`
-            );
-          }
-          return element?.name;
-        });
-
-        const transformColumnsIndex = Object
-          .values(entityModel.elements)
-          .filter(ele => TRANSPORT_CDS_TYPES.includes(ele.type))
-          .filter(ele => headers.includes(ele.name)) // only process columns in CSV files
-          .map(ele => ({
-            index: headers.indexOf(ele.name),
-            type: ele.type,
-            columnName: ele.name,
-          }));
-
-
-        const existedKeysIndex = headers
-          .filter(header => keys.includes(header))
-          .map(existedKey => headers.indexOf(existedKey));
-
-        if (existedKeysIndex.length !== keys.length) {
-          logger.warn(
-            "file", csvFile.yellow,
-            "do not provide enough primary keys",
-            keys,
-            "could not provision CSV, skip process"
-          );
-          continue;
-        }
-
-        if (transformColumnsIndex.length > 0) {
-          logger.debug(
-            "columns", transformColumnsIndex,
-            "in entity", entityName.green, "need to be transformed"
-          );
-          for (const entry of (rows as Array<Array<any>>)) {
-            for (const transformColumn of transformColumnsIndex) {
-              if (entry[transformColumn.index]?.trim?.().length > 0) {
-                switch (transformColumn.type) {
-                  // REVISIT: if binary as where condition, here will have issue.
-                  case "cds.Binary": case "cds.LargeBinary":
-                    entry[transformColumn.index] = Buffer.from(entry[transformColumn.index], "base64");
-                    break;
-                  case "cds.Integer":
-                    entry[transformColumn.index] = parseInt(entry[transformColumn.index], 10);
-                    break;
-                  case "cds.DateTime": case "cds.Timestamp":
-                    entry[transformColumn.index] = DateTime
-                      .fromISO(entry[transformColumn.index], { setZone: true })
-                      .toUTC()
-                      .toFormat(MYSQL_DATE_TIME_FORMAT);
-                    break;
-                  default:
-                    break;
-                }
-
+      if (transformColumnsIndex.length > 0) {
+        logger.debug(
+          "columns", transformColumnsIndex,
+          "in entity", entityName.green, "need to be transformed"
+        );
+        for (const entry of (rows as Array<Array<any>>)) {
+          for (const transformColumn of transformColumnsIndex) {
+            if (entry[transformColumn.index]?.trim?.().length > 0) {
+              switch (transformColumn.type) {
+                // REVISIT: if binary as where condition, here will have issue.
+                case "cds.Binary": case "cds.LargeBinary":
+                  entry[transformColumn.index] = Buffer.from(entry[transformColumn.index], "base64");
+                  break;
+                case "cds.Integer":
+                  entry[transformColumn.index] = parseInt(entry[transformColumn.index], 10);
+                  break;
+                case "cds.DateTime": case "cds.Timestamp":
+                  entry[transformColumn.index] = DateTime
+                    .fromISO(entry[transformColumn.index], { setZone: true })
+                    .toUTC()
+                    .toFormat(MYSQL_DATE_TIME_FORMAT);
+                  break;
+                default:
+                  break;
               }
+
             }
           }
         }
+      }
 
-        if (entires.length > 1) {
-          logger.info(
-            "filling entity", entityName.green,
-            "with file", path.relative(process.cwd(), csvFile).green,
-            "with", entires.length, "records"
-          );
-        } else {
-          logger.warn(
-            "file",
-            path.relative(process.cwd(), csvFile).yellow,
-            "is empty, skip processing"
-          );
-          continue;
+      if (entires.length > 1) {
+        logger.info(
+          "filling entity", entityName.green,
+          "with file", path.relative(process.cwd(), csvFile).green,
+          "with", entires.length, "records"
+        );
+      }
+      else {
+        logger.warn(
+          "file",
+          path.relative(process.cwd(), csvFile).yellow,
+          "is empty, skip processing"
+        );
+        continue;
+      }
+
+      const entryToWhereExpr = (entry: Array<string>) => {
+        const keyValues = [];
+
+        for (const existedKeyIndex of existedKeysIndex) {
+          keyValues.push(`${headers[existedKeyIndex]} = '${entry[existedKeyIndex]}'`);
         }
 
-        const entryToWhereExpr = (entry: Array<string>) => {
-          const keyValues = [];
-          for (const existedKeyIndex of existedKeysIndex) {
-            keyValues.push(`${headers[existedKeyIndex]} = '${entry[existedKeyIndex]}'`);
-          }
-
-          return keyValues.join(" AND ");
-        };
+        return keyValues.join(" AND ");
+      };
 
 
-        const batchInserts = [];
-        if (isPreDeliveryModel) {
-          headers.push("PreDelivery");
-        }
+      const batchInserts = [];
+      if (isPreDeliveryModel) {
+        headers.push("PreDelivery");
+      }
+
+      const sem = new Semaphore(10); // REVISIT: parameterized
+
+      await Promise.all(
+        rows.map(
+          entry => sem.use(async () => {
+            const keyExpr = entryToWhereExpr(entry);
+
+            const [[{ EXIST }]] = await connection
+              .query(`SELECT COUNT(1) as EXIST FROM ${tableName} WHERE ${keyExpr}`) as any;
+
+            if (EXIST === 0) {
+              if (isPreDeliveryModel) { entry.push(true as any); }
+              batchInserts.push(entry);
+            }
+            else {
+              // REVISIT: parameter: UPDATE or SKIP
+              logger.debug(
+                "entity", entityName,
+                "with key", keyExpr,
+                "has existed, skip process"
+              );
+            }
+          })
+        )
+      );
+
+
+      // batch insert
+      if (batchInserts.length > 0) {
+
         const headerList = headers.join(", ");
 
-        for (const entry of rows) {
+        logger.debug("batch inserts:", entityName, "with", batchInserts.length, "records");
 
-          const keyExpr = entryToWhereExpr(entry);
+        const [{ affectedRows }] = await connection
+          .query(`INSERT INTO ${tableName} (${headerList}) VALUES ?`, [batchInserts]) as any;
 
-          const [[{ EXIST }]] = await connection
-            .query(`SELECT COUNT(1) as EXIST FROM ${tableName} WHERE ${keyExpr}`) as any;
-
-          if (EXIST === 0) {
-            if (isPreDeliveryModel) { entry.push(true as any); }
-            batchInserts.push(entry);
-          }
-          else {
-            // REVISIT: parameter: UPDATE or SKIP
-            logger.debug("entity", entityName, "with key", keyExpr, "has existed, skip process");
-          }
-
+        if (affectedRows !== batchInserts.length) {
+          logger.warn(
+            "batch insert records for entity", entityName,
+            "with", batchInserts.length, "records",
+            "but db affectRows is", affectedRows,
+            "please CARE and CHECK the reason"
+          );
         }
-
-        // batch insert
-        if (batchInserts.length > 0) {
-
-          logger.debug("batch inserts:", entityName, "with", batchInserts.length, "records");
-
-          const [{ affectedRows }] = await connection
-            .query(`INSERT INTO ${tableName} (${headerList}) VALUES ?`, [batchInserts]) as any;
-
-          if (affectedRows !== batchInserts.length) {
-            logger.warn(
-              "batch insert records for entity", entityName,
-              "with", batchInserts.length, "records",
-              "but db affectRows is", affectedRows,
-              "please CARE and CHECK the reason"
-            );
-          }
-
-        }
-
 
       }
-      await connection.commit();
-      logger.info("provision CSV finished");
-    }
-    catch (error) {
-      await connection.rollback();
-      logger.error("provision CSV failed:", error);
-      throw error;
-    }
-    finally {
-      await connection.end();
-    }
-  }
 
+
+    }
+    await connection.commit();
+    logger.info("provision CSV finished");
+  }
+  catch (error) {
+    await connection.rollback();
+    logger.error("provision CSV failed:", error);
+    throw error;
+  }
+  finally {
+    await connection.end();
+  }
 
   logger.info("CSV provision data migration successful");
 
