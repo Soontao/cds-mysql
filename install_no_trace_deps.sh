4928069d16340353b33eba1fd3ce01ba49b04459
Theo Sun <theo.sun@outlook.com>
2023-07-24 21:57:37 +0800

    chore: update script

--- install_no_trace_deps.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ ! -d "node_modules/@sap/cds" ]; then
-  echo "Installing no trace dependencies ...";
-  CDS_VERSION=$(node -e "console.log(require('./src/cds.version').VERSION)");
-  MTXS_VERSION=$(node -e "console.log(require('./src/cds.version').MTXS_VERSION)");
-  npm i --no-save express @sap/cds@$CDS_VERSION @sap/cds-dk@$CDS_VERSION @sap/cds-mtxs@$MTXS_VERSION sqlite3;
-fi
---
+++ install_optional_deps.js
@@ -0,0 +1,24 @@
+
+const { spawn } = require("child_process");
+const fs = require("fs");
+const { platform } = require("os");
+const path = require("path");
+
+if (!fs.existsSync(path.join(__dirname, "./node_modules/@sap/cds"))) {
+  console.log("Installing no trace dependencies ...");
+  const { VERSION, MTXS_VERSION } = require("./src/cds.version");
+  const deps = [
+    `@sap/cds@${VERSION}`, `@sap/cds-dk@${VERSION}`, `@sap/cds-mtxs@${MTXS_VERSION}`, `sqlite3`
+  ];
+
+  const p = spawn(
+    `npm i --no-save express ${deps.join(" ")}`,
+    {
+      shell: platform() !== "win32",
+    }
+  );
+  p.stdout.pipe(process.stdout);
+  p.stderr.pipe(process.stderr);
+  p.on("error", console.error);
+  p.on("exit", () => console.log("done"));
+}
